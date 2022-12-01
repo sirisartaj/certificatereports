6dc8a79eeeaf8326fc582e2ac128c4d3087c76d3
sirisartaj <116791477+sirisartaj@users.noreply.github.com>
2022-12-01 23:27:30 +0530

    bug fixed
    from date todate fixed

--- app/Controllers/CertificateController.php
+++ app/Controllers/CertificateController.php
@@ -12,10 +12,10 @@ class CertificateController extends Controller
             $data['from_date']= $this->request->getVar('from_date');
             $data['to_date']= $this->request->getVar('to_date');
             if($data['from_date']==''){
-                $data['from_date'] = date('Y-m-d H:i:s', strtotime('-1 month'))
+                $data['from_date'] = date('Y-m-d H:i:s', strtotime('-1 month'));
             } 
             if($data['to_date']==''){
-                $data['to_date'] = date('Y-m-d H:i:s')
+                $data['to_date'] = date('Y-m-d H:i:s');
             }    
         }else{            
             $data['from_date']= date('Y-m-d H:i:s', strtotime('-1 month'));
