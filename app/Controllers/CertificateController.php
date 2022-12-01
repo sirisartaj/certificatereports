789b222a06a77863f1eb717ef293b23c42f30c33
sirisartaj <116791477+sirisartaj@users.noreply.github.com>
2022-12-01 23:26:38 +0530

    from date todate fixed
    validation for from and todate

--- app/Controllers/CertificateController.php
+++ app/Controllers/CertificateController.php
@@ -11,7 +11,12 @@ class CertificateController extends Controller
         if($this->request->getVar()){
             $data['from_date']= $this->request->getVar('from_date');
             $data['to_date']= $this->request->getVar('to_date');
-                        
+            if($data['from_date']==''){
+                $data['from_date'] = date('Y-m-d H:i:s', strtotime('-1 month'))
+            } 
+            if($data['to_date']==''){
+                $data['to_date'] = date('Y-m-d H:i:s')
+            }    
         }else{            
             $data['from_date']= date('Y-m-d H:i:s', strtotime('-1 month'));
             $data['to_date']= date('Y-m-d H:i:s');            
